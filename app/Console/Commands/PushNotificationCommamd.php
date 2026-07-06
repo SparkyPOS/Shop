9ca1f55a84af2a644c1f76f462c8ea96101ed6ac
SparkyPOS <155199971+SparkyPOS@users.noreply.github.com>
2026-07-06 17:37:12 -0400

    changes

--- app/Console/Commands/PushNotificationCommamd.php
+++ app/Console/Commands/PushNotificationCommamd.php
@@ -6,7 +6,7 @@ use App\Models\DeviceToken;
 use App\Models\PushNotification;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Http;
-use Str;
+use Illuminate\Support\Str;
 class PushNotificationCommamd extends Command
 {
     /**
@@ -40,36 +40,42 @@ class PushNotificationCommamd extends Command
      */
     public function handle()
     {
-        $id = auth('api')->user()->id();
-       $notifications = PushNotification::where('push_send_type',0)->with('user_notification_setting')->get();
-       $devices = DeviceToken::where('user_id',$id)->get();
+        $notifications = PushNotification::where('push_send_type',0)->with('user_notification_setting')->get();
 
-       foreach ($notifications as $notification) {
-        if (Str::contains(@$notifications->user_notification_setting->type, 'mobile')) {
+        foreach ($notifications as $notification) {
+            if (!Str::contains(@$notifications->user_notification_setting->type, 'mobile')) {
+                continue;
+            }
+            if(!$notification->user_id) {
+                $notification->delete();
+                continue;
+            }
+            $devices = DeviceToken::where('user_id', $notification->user_id)->get();
+
             foreach ($devices as $device) {
                 Http::withToken(env('FCM_SERVER_KEY'))
-                ->post('https://fcm.googleapis.com/fcm/send', [
-                    "to" => $device->device_token,
-                    "notification" => [
-                        "priority" => "high",
-                        "title" => $notification->title,
-                        "body" => $notification->body,
-                        "type" =>  $notification->type,
-                        "click_action" => "FLUTTER_NOTIFICATION_CLICK", 
-                    ],
-                    "data" => [
-                        "priority" => "high",
-                        "title" => $notification->title,
-                        "body" => $notification->body, 
-                        "type" =>  $notification->type, 
-                        "click_action" => "FLUTTER_NOTIFICATION_CLICK",
-                    ],
-                ]);
+                    ->post('https://fcm.googleapis.com/fcm/send', [
+                        "to" => $device->device_token,
+                        "notification" => [
+                            "priority" => "high",
+                            "title" => $notification->title,
+                            "body" => $notification->body,
+                            "type" =>  $notification->type,
+                            "click_action" => "FLUTTER_NOTIFICATION_CLICK", 
+                        ],
+                        "data" => [
+                            "priority" => "high",
+                            "title" => $notification->title,
+                            "body" => $notification->body, 
+                            "type" =>  $notification->type, 
+                            "click_action" => "FLUTTER_NOTIFICATION_CLICK",
+                        ],
+                    ]);
             }
-           
+                
+            $notification->delete();
         }
-        $notification->delete();
-       }
         return true;
     }
+    return Command::SUCCESS;
 }
